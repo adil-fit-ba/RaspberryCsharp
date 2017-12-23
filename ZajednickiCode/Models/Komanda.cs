788e277f013a1ac47a21c8c4aee97f1511a52707
Adil Joldić <adil@edu.fit.ba>
2017-12-23 13:54:00 +0100

    12:53

---
+++ ZajednickiCode/Models/Komanda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Komanda
+    {
+        public int Id { get; set; }
+
+        public bool JelIzvrsena { get; set; }
+
+        public int Code { get; set; }
+    }
+}
