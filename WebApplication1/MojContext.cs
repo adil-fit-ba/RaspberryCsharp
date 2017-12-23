2a4fb5a204d3efda7f6765d6852f7914641e2875
Adil Joldić <adil@edu.fit.ba>
2017-12-23 14:17:07 +0100

    aa

--- WebApplication1/MojContext.cs
+++ WebApplication1/MojContext.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
     public class MojContext:DbContext
     {
+        public MojContext():base("Server=app.fit.ba,1433;Database=RS1_online1;Trusted_Connection=false;MultipleActiveResultSets=true;User ID=nekiUser;Password=nekiPass")
+        {
+            
+        }
+        public DbSet<Komanda> komanda { get; set; }
     }
 }
---
+++ WebApplicationSaCore/MojContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class MojContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=app.fit.ba,1433;Database=RS1_online1;Trusted_Connection=false;MultipleActiveResultSets=true;User ID=nekiUser;Password=nekiPass");
+        }
+
+        public DbSet<Komanda> komanda { get; set; }
+    }
+}
