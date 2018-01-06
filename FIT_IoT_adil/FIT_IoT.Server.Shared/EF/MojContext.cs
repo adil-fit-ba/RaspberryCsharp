9cfdb23244265698ac281e81b1128852152b6dfd
Adil Joldić <adil@edu.fit.ba>
2018-01-06 12:45:35 +0100

    adil

---
+++ FIT_IoT_adil/FIT_IoT.Server.Shared/EF/MojContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using FIT_IoT.Server.Shared.EntityModel;
+
+namespace WebApplication1
+{
+    public class MojContext:DbContext
+    {
+        public MojContext():base("Server=app.fit.ba,1432;Database=iot1;Trusted_Connection=false;MultipleActiveResultSets=true;User ID=iot1;Password=Mostar2017")
+        {
+            
+        }
+        public DbSet<Komanda> komanda { get; set; }
+    }
+}
---
+++ FIT_IoT_adil/FIT_IoT.Server.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT_IoT.Server.Web.Global" Language="C#" %>
